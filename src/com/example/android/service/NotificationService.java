70e2dc5af0894e43e9d38f107ae28240cde7c142
Nishant Patil <nishant@webaroo.com>
2013-12-11 06:23:10 +0530

    Even if time has passed, app should only give notification at 6am while loading for first time.   Need improvement here.

--- src/com/example/android/service/NotificationService.java
+++ src/com/example/android/service/NotificationService.java
@@ -1,5 +1,7 @@
 package com.example.android.service;
 
+import java.util.Calendar;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
@@ -21,7 +23,17 @@ public class NotificationService extends Service
 	public void onCreate()
 	{
 		super.onCreate();
-		NotifManager.notify(this);
+		
+		//TODO : Nishant : Adding this at last moment. Need to keep it at right place.
+		// Time : 06:20AM 11Dec.
+		
+		Calendar cal=Calendar.getInstance();
+	    cal.set(Calendar.HOUR_OF_DAY, 06);
+	    cal.set(Calendar.MINUTE,00);
+	    cal.set(Calendar.SECOND, 00);
+	    if(System.currentTimeMillis() <= cal.getTimeInMillis()) {
+	    	NotifManager.notify(this);
+	    }
 	}
 
 	@Override
